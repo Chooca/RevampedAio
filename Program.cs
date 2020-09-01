a0772740d78c6d5e6c16cf14efc3fb69f3aea533
Czechux7 <choocas0@gmail.com>
2020-09-01 19:24:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnsoulSharp;
+using EnsoulSharp.SDK;
+
+namespace RevampedAio
+{
+    class Program
+    {
+        private static AIHeroClient Player => ObjectManager.Player;
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += GameEventOnOnGameLoad;
+        }
+
+        private static void GameEventOnOnGameLoad()
+        {
+            switch (Player.CharacterName)
+            {
+                case "Cassiopeia":
+                    Cassiopeia_Du_Couteau_2.Program.CassiopeiaLoading_OnLoadingComplete();
+                    break;
+            }
+        }
+    }
+}
