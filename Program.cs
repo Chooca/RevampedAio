3601c670b5c4005549f5c4c120d54d3a24d4ff43
Czechux7 <choocas0@gmail.com>
2020-09-01 20:38:20 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using EnsoulSharp;
 using EnsoulSharp.SDK;
 
@@ -25,5 +27,27 @@ namespace RevampedAio
                     break;
             }
         }
+        public static void Check()
+        {
+            try
+            {
+
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                bool wb = new WebClient().DownloadString("https://github.com/Chooca/RevampedAio/blob/master/version.txt").Contains("0.0.0.1");
+                if (!wb)
+                {
+                    Game.Print("<b><font Size='25' color='#0000b2'>RevampedAIO outdated version...Please Update!</font></b>");
+                }
+                else
+                    Game.Print("<b><font Size='35' color='#FF0000'>RevampedAIO loaded, current version(0.0.0.1)</font></b>");
+
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine("An error try again " + E);
+            }
+        }
+
+
     }
 }
